3aa204f9c88abd4a802c86df0894213e9c9c2e1e
Rohith77o <143720540+Rohith77o@users.noreply.github.com>
2025-02-12 11:52:40 +0530

    Add files via upload

---
+++ Shortcircuit (2).java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Shortcirucuit
+{
+public static void main(String[] args )
+{
+int  a=10, b=20,c=30;
+if(a>b&&a>c){
+    System.out.println("A is greater");
+}
+else if(b>c){
+    System.out.println("B is greater");
+}
+else{
+    System.out.println("C is greater");
+}
+}
+}
