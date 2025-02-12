960b855abac3c83c76e6f3d85817a57b115cc869
Rohith77o <143720540+Rohith77o@users.noreply.github.com>
2025-02-12 12:07:22 +0530

    Update Ternary.java

--- Ternary.java
+++ Ternary.java
@@ -4,9 +4,9 @@ public class Ternary
 {
 public static void main(String[] args )
 {
-int a = 10,b = 23;
+int a = 10,b = 23,c=30;
 int max;
-max =(a>b)?a:b;
+max =(a>b)&&(a>c)?a:(b>c)?b:c;
 System.out.println(max);
 }
 }
