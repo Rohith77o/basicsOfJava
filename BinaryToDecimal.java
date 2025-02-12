f20e5b2b290c62c11f9367ce131ac01aad96e3ec
Rohith77o <143720540+Rohith77o@users.noreply.github.com>
2025-02-12 12:38:36 +0530

    Add files via upload

---
+++ BinaryToDecimal.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class BinaryToDecimal
+{
+public static void main(String[] args )
+{
+int a = 492, b = 333;
+int c = a&b;
+int d = a|b;
+int e = a^b;
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+}
+}
