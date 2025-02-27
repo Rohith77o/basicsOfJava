5e634bed3e352606f918be0cb5be47f074bd2d1a
Rohith77o <143720540+Rohith77o@users.noreply.github.com>
2025-02-27 10:20:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
 
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+public class Main {
+
+    public static boolean isAutomorphic(int num) {
+        int square = num * num; 
+        int temp = num; 
+        
+            while (temp > 0) {
+            if (square % 10 != temp % 10) { 
+                return false; 
+            }
+            temp /= 10; 
+            square /= 10; 
+        }
+        
+        return true; 
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt(); 
+        if (isAutomorphic(num)) { 
+            System.out.println(num + " is an automorphic number"); 
+        } else {
+            System.out.println(num + " is not an automorphic number"); 
+        }
+    }
 }
